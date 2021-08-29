fba8135318f53e924677b655e8d816fb1a96b91c
Paz Spera <mspera03@gmail.com>
2021-08-28 21:57:52 -0300

    prueba loop varias game_cards

--- main.js
+++ main.js
@@ -2,24 +2,39 @@ const app = Vue.createApp({
     data(){
         return {
             name: 'Board Game Tracker',
-            /* game_cards: [
+            game_cards: [
                 {
                     id: 1,
-                    card_title: 'Dominion',
-                    card_date: '28/08/2021'
+                    game: 'Dominion',
+                    date: '28/08/2021',
+                    players: [
+                        { id: 1, player_name: 'Zeuchi', winner: true },
+                        { id: 2, player_name: 'Marea', winner: false}
+                    ]
                 },
                 {
                     id: 2,
-                    card_title: 'Star Realms',
-                    card_date: '15/07/2021',
+                    game: 'Star Realms',
+                    date: '15/07/2021',
+                    players: [
+                        { id: 1, player_name: 'Zeuchi', winner: true },
+                        { id: 2, player_name: 'Marea', winner: false}
+                    ]
                 }
-            ], */
-            card_title: 'Dominion',
+            ],
+            /* card_title: 'Dominion',
             card_date: '28/08/2021',
             card_players: [
                 { id: 1, player_name: 'Zeuchi', winner: true },
                 { id: 2, player_name: 'Marea', winner: false}
-            ]
+            ] */
+            methods: {
+                loopArray(array){
+                    for(element of array){
+                        return element.player_name;
+                    }
+                }
+            }
         }
     }
 });
