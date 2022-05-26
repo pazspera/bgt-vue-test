34f6b319f06ee74a3e0327459b9de39f274886c4
Paz Spera <mspera03@gmail.com>
2022-05-26 03:09:05 -0300

    saqué el navbar de App.vue y lo guardé en su propio componente NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,105 @@
+<template>
+  <header class="sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <router-link class="navbar-brand" to="/"><img src="@/assets/img/logo_dark.png" /></router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/juegos">Juegos</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/jugadores">Jugadores</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+
+/*  Header  */
+.navbar {
+  background-color: $black !important;
+}
+
+.nav-link {
+  font-size: $size5;
+  font-weight: 700;
+  font-family: $font-text;
+}
+
+/*  Agrega margin a los nav-links en desktop */
+@media (min-width: 996px) {
+  .nav-item {
+    margin-left: 1.2rem !important;
+  }
+}
+
+// Override color a los nav-links
+.navbar-dark,
+.navbar-brand,
+.navbar-nav,
+.nav-link {
+  color: $white !important;
+  text-transform: uppercase;
+  transition: all 0.5s ease-in-out;
+
+  &:hover,
+  &:focus {
+    color: $primary-medium !important;
+    transition: all 0.5s ease-in-out;
+  }
+}
+
+// Color para mostrar página activa en navbar
+.active {
+  font-weight: 700;
+  color: $white !important;
+}
+
+// Override color del navbar-toggler
+.navbar-dark,
+.navbar-toggler {
+  color: $white;
+
+  &:hover,
+  &:focus {
+    color: $primary-medium;
+  }
+}
+
+// Override color de navbar-dark
+.bg-dark {
+  background-color: $black !important;
+}
+
+// Anchor para arreglar padding link menu
+.anchor {
+  display: block;
+  height: 79px;
+  margin-top: -79px;
+  visibility: hidden;
+}
+</style>
+
