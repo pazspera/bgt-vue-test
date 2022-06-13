214e87c22fe38c3b484078d2a998a2bcd5fbbf7a
Puchacz <puchaczdor@gmail.com>
2022-06-13 17:15:33 -0300

    configuro proxy para el webserver de vue y agrego un fetch a la API

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -3,13 +3,48 @@
     <div class="row">
       <div class="col">
         <h1>Soy el test component, estoy en el Home</h1>
+
+        <p>Board game list</p>
+        <ul>
+          <li v-for="boardGame in boardGames" :key="boardGame.id">
+            {{ boardGame.name }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      boardGames: null,
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        fetch(`/boardgame/all`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((messages) => {
+            console.log("messages: ", messages);
+            this.boardGames = messages;
+          });
+      } catch (err) {
+        console.error("err", err);
+      }
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
